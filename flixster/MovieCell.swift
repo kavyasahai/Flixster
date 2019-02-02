00035663c4916ef3ff25ed84c402b59410f970b5
Kavya Sahai <kavyamohan.sahai@sjsu.edu>
2019-02-02 15:24:57 -0800

    Cell added.

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Kavya Sahai on 2/2/19.
+//  Copyright © 2019 Kavya Sahai. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
